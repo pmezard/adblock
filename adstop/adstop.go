7660ce5bdd69574fadceab045df5dcbd94e575ed
Patrick Mezard <patrick@mezard.eu>
2015-01-02 16:47:27 +0100

    adstop: document the proxy and expected network configuration

--- adstop/adstop.go
+++ adstop/adstop.go
@@ -1,3 +1,33 @@
+/*
+adstop is an ad-blocking transparent HTTP/HTTPS proxy.
+
+It was designed to run on low power, low memory ARM devices and serve a couple
+of clients, mostly old smartphones which cannot run adblockers themselves.
+
+Before using it, you have to configure your devices and network to make it
+accessible as a transparent proxy. One way to achieve this is to install
+a VPN on the server side and redirect all HTTP/HTTPS traffic to the proxy
+with routing rules. Then make the client browse through the VPN.
+
+	$ adstop -http localhost:1080 \
+		-https localhost:1081     \
+		-cache .adstop			  \
+		-max-age 24h			  \
+		https://easylist-downloads.adblockplus.org/easylist.txt \
+		some_local_list.txt
+
+starts the proxy and makes it listen on HTTP on port 1080, HTTPS on port 1081,
+fetch and load rules from easylist and a local file, cache easylist in an
+.adstop/ directory and refresh it every 24 hours.
+
+Note that HTTPS filtering requires the proxy to intercept the device traffic
+and decrypt it. To allow this, you have to add the proxy certificate authority
+to your device. By default, adstop uses goproxy ca.pem file but you should
+generate your own to avoid opening your device communications to third parties.
+
+TODO: configure the CA on command line to avoid recompiling goproxy with our
+own.
+*/
 package main
 
 import (
